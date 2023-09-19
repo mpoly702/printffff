ee2716e0e784d2a0251fd05512beab73b30226d0
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 20:21:16 +0100

    Update prnt_2-printf.c

--- test3/prnt_2-printf.c
+++ test3/prnt_2-printf.c
@@ -26,7 +26,7 @@ int print_hex(va_list ap, params_t *params)
 		*--str = 'x';
 		*--str = '0';
 	}
-	params->unsign = 1;
+	params->usg = 1;
 	return (c += print_number(str, params));
 }
 
@@ -56,7 +56,7 @@ int print_HEX(va_list ap, params_t *params)
 		*--str = 'X';
 		*--str = '0';
 	}
-	params->unsign = 1;
+	params->usg = 1;
 	return (c += print_number(str, params));
 }
 /**
@@ -74,7 +74,7 @@ int print_binary(va_list ap, params_t *params)
 
 	if (params->hashtag_flag && n)
 		*--str = '0';
-	params->unsign = 1;
+	params->usg = 1;
 	return (c += print_number(str, params));
 }
 
@@ -101,6 +101,6 @@ int print_octal(va_list ap, params_t *params)
 
 	if (params->hashtag_flag && l)
 		*--str = '0';
-	params->unsign = 1;
+	params->usg = 1;
 	return (c += print_number(str, params));
 }
