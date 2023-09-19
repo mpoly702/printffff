5763d59c9b2ea06b91f72ede33a2b6f62dd4c2e1
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 17:20:05 +0100

    Update printf_numbs.c

--- test3/printf_numbs.c
+++ test3/printf_numbs.c
@@ -18,7 +18,7 @@ char *convert(long int num, int base, int flags, params_t *params)
 	unsigned long n = num;
 	(void)params;
 
-	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	if (!(flags & UNSIGNED_CONV) && num < 0)
 	{
 		n = -num;
 		sign = '-';
@@ -55,7 +55,7 @@ int print_unsigned(va_list ap, params_t *params)
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
 	params->unsign = 1;
-	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+	return (print_number(convert(l, 10, UNSIGNED_CONV, params), params));
 }
 
 
@@ -76,7 +76,7 @@ int print_address(va_list ap, params_t *params)
 	if (!n)
 		return (_puts("(nil)"));
 
-	str = convert(n, 16, CONVERT_UNSIGNED | LOWER_CASE, params);
+	str = convert(n, 16, UNSIGNED_CONV | LOWER_CASE, params);
 	*--str = 'x';
 	*--str = '0';
 	return (print_number(str, params));
