0194df91fed0450f08b7acb8c80dfef9d802e6ae
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 13:38:01 +0100

    Update _printf.c

--- test/_printf.c
+++ test/_printf.c
@@ -18,25 +18,24 @@ int _printf(const char * const format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-while (format[i] != '\0') {
-        if (format[i] == '%') {
-            i++;
-            int j = 0;
-            while (j < sizeof(m) / sizeof(m[0])) {
-                if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1]) {
-                    len += m[j].f(args);
-                    i = i + 2;
-                    break;  // Exit the inner loop when a match is found
-                }
-                j++;
-            }
-        } else {
-            _putchar(format[i]);
-            len++;
-            i++;
-        }
-    }
-
-    va_end(args);
-    return len;
+Here:
+while (format[i] != '\0')
+{
+j = 13;
+while (j >= 0)
+{
+if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+{
+len += m[j].f(args);
+i = i + 2;
+goto Here;
+}
+J--;
+}
+_putchar(format[i]);
+Len++;
+I++;
+}
+va_end(args);
+return (len);
 }
