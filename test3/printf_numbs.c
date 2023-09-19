33efd5e559bcd66862972539a289934b5c15bddb
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 19:54:11 +0100

    Update printf_numbs.c

--- test3/printf_numbs.c
+++ test3/printf_numbs.c
@@ -54,7 +54,7 @@ int print_unsigned(va_list ap, params_t *params)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
-	params->unsign = 1;
+	params->usg = 1;
 	return (print_number(convert(l, 10, UNSIGNED_CONV, params), params));
 }
 
