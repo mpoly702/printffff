e45885af9c5499f21716df78924f2ee34b59989d
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 21:47:22 +0100

    Update _printf.c

--- test3/_printf.c
+++ test3/_printf.c
@@ -41,7 +41,7 @@ int _printf(const char *format, ...)
 			p++;
 		if (!get_specifier(p))
 			sum += print_from_to(start, p,
-					params.l_modifier || params.h_modifier ? p - 1 : 0);
+					params.modi_l || params.modi_h ? p - 1 : 0);
 		else
 			sum += get_print_func(p, ap, &params);
 	}
