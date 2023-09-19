83d58b8f6bb677b345c6f111dcf6a1790464d9a0
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-19 14:33:29 +0100

    Update main.h

--- test3/main.h
+++ test3/main.h
@@ -7,17 +7,17 @@
 #include <limits.h>
 #include <stdlib.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#define LOWER_CASE	1
+#define CONVERT_UNSIGNED	2
 
-#define FIELD_BUF_SIZE 50
+#define BUF_FLUSH -1
+#define OUTPUT_BUF_SIZE 1024
 
 #define NULL_STRING "(null)"
 
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
+#define FIELD_BUF_SIZE 50
 
 /**
  * struct parameters - parameters struct
