b9cb92c0d61f2be7ee57ff99a590dfa582463705
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-21 01:14:04 +0100

    Update _printf.c

--- test4/_printf.c
+++ test4/_printf.c
@@ -15,29 +15,27 @@ int _printf(const char * const format, ...)
     va_start(args, format);
 	
 	char *formatPtr = (char *)format;
-    while (*formatPtr) 
+	int i;
+    for (i = 0; format[i] != '\0'; i++) 
     {
-        if (*formatPtr == '%') 
-	{
-            formatPtr++; /*Move past the '%'*/
-		
-		int i;
-            for (i = 0; i < 2; i++) 
-	    if (*formatPtr == 's') 
-	    {
-                    Print_String(args);
-                    break;
-	    } else if (*formatPtr == 'c') 
-	    {
-                    Print_Char(args);
-                    break;
-	    }
-            else 
-	    {
-            _putchar(*formatPtr);
-	    }
-        	formatPtr++; /* Move to the next character in the format string */
-	}
+	     if (format[i] == '%')/* if a directive ('%') is encountered */
+	     {
+		     i++; /* Move to the next character after '%' */
+		     if (format[i] == 's')
+		     {
+			     Print_String(args);
+			     break;
+		     } else if (format[i] == 'c')
+		     {
+			     Print_Char(args);
+			     break;
+		     }
+		     else 
+		     {
+			     _putchar(format[i]);
+		     }		
+        	format[i + 1]; /* Move to the next character in the format string */
+		}
     }
 	va_end(args);
 }
