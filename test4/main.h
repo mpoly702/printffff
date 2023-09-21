f70103f30ef90a1536ea4e5bbbdd74f0212da459
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-21 03:41:34 +0100

    Update main.h

--- test4/main.h
+++ test4/main.h
@@ -7,43 +7,7 @@
 #include <limits.h>
 #include <unistd.h>
 
-#define NULL_STRING "(null)"
 
-/**
- * struct parameters - parameters struct
- *
- * @unsign: flag if unsigned value
- *
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- *
- * @width: field width specified
- * @precision: field precision specified
- *
- * @h_modifier: on if h_modifier is specified
- * @l_modifier: on if l_modifier is specified
- *
- */
-
-typedef struct parameters
-{
-	unsigned int unsign		: 1;
-
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
-
-	unsigned int width;
-	unsigned int precision;
-
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-} params_t;
 
 struct SpecifierFunctionMap 
 {
@@ -53,7 +17,7 @@ struct SpecifierFunctionMap
 
 int _putchar(char c);
 int _printf(const char * const format, ...);
-void Print_String(va_list args, params_t *params);
+void Print_String(va_list args);
 void Print_Char(va_list args);
 size_t _strlen(const char *str);
 
