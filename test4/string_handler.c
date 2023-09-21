9ddc1c41397f78a503cd8811be78b935679f223d
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-21 03:40:08 +0100

    Update string_handler.c

--- test4/string_handler.c
+++ test4/string_handler.c
@@ -1,62 +1,29 @@
 #include "main.h"
-
 /**
- * Print_String - this function prints a string
- * @args: the argument
- *
+ * printf_string - print a string.
+ * @val: argumen t.
  * Return: the length of the string.
  */
-int Print_String(va_list args, params_t *params)
+
+int printf_string(va_list args)
 {
-    char *str = va_arg(args, char *), pad_char = ' ';
-    unsigned int pad = 0, sum = 0, i = 0, j;
+	char *s;
+	int i, len;
 
-    (void)params; /*This line is used to suppress the "unused parameter" warning.*/
-
-    /*Switch statement to handle the case where str is NULL*/
-    switch ((int)(!str))
-    {
-        case 1:
-            str = NULL_STRING; /*If str is NULL, set it to a predefined NULL_STRING*/
-    }
-
-    j = pad = _strlen(str); /*Initialize j and pad with the length of the string*/
-
-    /*Adjust j and pad based on precision (if specified)*/
-    if (params->precision < pad)
-        j = pad = params->precision;
-
-    if (params->minus_flag)
-    {
-        /*Handle left-justified printing with precision or without.*/
-        if (params->precision != UINT_MAX)
-        {
-            for (i = 0; i < pad; i++)
-                sum += _putchar(*str++);
-        }
-        else
-        {
-            sum += _puts(str);
-        }
-    }
-
-    /*Fill any remaining width with pad characters (spaces) if right-justified.*/
-    while (j++ < params->width)
-        sum += _putchar(pad_char);
-
-    if (!params->minus_flag)
-    {
-        /*Handle right-justified printing with precision or without.*/
-        if (params->precision != UINT_MAX)
-        {
-            for (i = 0; i < pad; i++)
-                sum += _putchar(*str++);
-        }
-        else
-        {
-            sum += _puts(str);
-        }
-    }
-
-    return (sum); /*Return the total number of characters printed.*/
+	s = va_arg(val, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
 }
