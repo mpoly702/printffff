3cf804a03298af525dd36bc3486edd4b56000c97
Godfrey <mpoly702@gmail.com>
2023-10-18 23:53:25 +0100

    betty

---
+++ shell/shell.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define BUF 1024
+
+char **tokenizer(char *string);
+int main(int argc, char **argv[]);
+
+#endif
