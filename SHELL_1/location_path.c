c3a29d474dcf74e10b763b1072218080530e1d7d
Grace0615 <graceebruwiyor@gmail.com>
2023-10-20 18:26:43 +0100

    location

--- SHELL_1/location_path.c
+++ SHELL_1/location_path.c
@@ -9,38 +9,34 @@
  */
 char *get_path(char *command)
 {
-    char *path = NULL;
-    struct stat st;
-    char *path_copy = NULL;
-    char *token;
-    char *temp;
+char *path = NULL;
+struct stat st;
+char *path_copy = NULL;
+char *token;
+char *temp;
 
+path = getenv("PATH");
+if (path == NULL || path[0] == '\0')
+{
+return (NULL);
+}
+path_copy = strdup(path); /*Make a copy of PATH for modification*/
+token = strtok(path_copy, ":"); /*Split the PATH by colons*/
+while (token != NULL)
+{
+temp = (char *)malloc(strlen(token) + strlen(command) + 2);
+strcpy(temp, token);
+strcat(temp, "/");
+strcat(temp, command);
 
-    path = getenv("PATH");
-
-    if (path == NULL || path[0] == '\0') {
-        return NULL;
-    }
-
-    path_copy = strdup(path); /*Make a copy of PATH for modification*/
-    token = strtok(path_copy, ":"); /*Split the PATH by colons*/
-
-    while (token != NULL) {
-        temp = (char *)malloc(strlen(token) + strlen(command) + 2);
-        strcpy(temp, token);
-        strcat(temp, "/");
-        strcat(temp, command);
-
-        if (stat(temp, &st) == 0) 
-	{
-            free(path_copy); /*Free the copy of PATH*/
-            return strdup(temp); /*Return a duplicate of the found path*/
-        }
-
-        token = strtok(NULL, ":");
-    }
-
-    free(path_copy); /*Free the copy of PATH*/
-    return NULL;
+if (stat(temp, &st) == 0)
+{
+free(path_copy); /*Free the copy of PATH*/
+return (strdup(temp)); /*Return a duplicate of the found path*/
+}
+token = strtok(NULL, ":");
+}
+free(path_copy); /*Free the copy of PATH*/
+return (NULL);
 }
 
